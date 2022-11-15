8f167fd95278c3a678776d93d864dfb7283505c7
Cornelius Heggli <cornelius.heggli@gmail.com>
2022-11-15 16:46:30 +0800

    Feat: complete coaching offer new, incl. controller actions

--- app/controllers/coaching_offers_controller.rb
+++ app/controllers/coaching_offers_controller.rb
@@ -1,5 +1,21 @@
 class CoachingOffersController < ApplicationController
   def new
-    @offer = CoachingOffer.new
+    @coaching_offer = CoachingOffer.new
+  end
+
+  def create
+    @coaching_offer = CoachingOffer.new(coaching_offer_params)
+    @coaching_offer.user = current_user
+    if @coaching_offer.save
+      redirect_to coaching_offer_path(@coaching_offer)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def coaching_offer_params
+    params.require(:coaching_offer).permit(:skill, :description, :price)
   end
 end
