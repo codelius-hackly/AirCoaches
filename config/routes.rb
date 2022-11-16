6470f332fd4d2a89ddec5e4a74a9b7c2d5fd4b8b
Eliska Knop Kostkova <e.knopkostkova@gmail.com>
2022-11-16 12:22:44 +0800

    showpage done

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   root to: "pages#home"
   devise_for :users
 
-  resources :coaching_offers, only: [:show, :new, :create]
+  resources :coaching_offers, only: [:show, :new, :create] do
+    resources :bookings
+  end
 end
