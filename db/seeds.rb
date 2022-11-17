8038889febc7ab37d3015ea7750aa8b1b032936b
Cornelius Heggli <cornelius.heggli@gmail.com>
2022-11-17 15:29:42 +0800

    didn't do much, only comment

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,7 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 puts "Destroying existing Data..."
+# TODO: Implement ActiveStorage purge for cloudinary images for every db
 Booking.destroy_all
 CoachingOffer.destroy_all
 User.destroy_all
