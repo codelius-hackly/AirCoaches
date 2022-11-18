5f904be20753b8c24f449315d85fea242bbd0d91
Becis Redoune <b6red1@gmail.com>
2022-11-18 13:15:21 +0800

    add status

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_17_035812) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_18_050432) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -50,6 +50,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_17_035812) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status", default: "pending"
     t.index ["coaching_offer_id"], name: "index_bookings_on_coaching_offer_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
