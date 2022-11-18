215689efb11384242635a380117c06854c08c6e1
Gabriela Felder <gab.felder@bluewin.ch>
2022-11-18 13:11:07 +0800

    Fixed display of bookings in dashboard

--- db/schema.rb
+++ db/schema.rb
@@ -87,6 +87,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_17_035812) do
     t.string "first_name"
     t.string "last_name"
     t.text "profile_description"
+    t.string "skills"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
